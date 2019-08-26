1a9a93fcb9caada823db806ae057a7aa351f300e
Vo Thanh Tuan <tuan.vt@tctav.com>
2019-08-26 14:45:35 +0700

    Use environment variables in our functions

--- create.js
+++ create.js
@@ -5,15 +5,16 @@ import { success, failure } from "./libs/response-lib";
 export async function main(event, context) {
   const data = JSON.parse(event.body);
   const params = {
-    TableName: "notes",
-    Item: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: uuid.v1(),
-      content: data.content,
-      attachment: data.attachment,
-      createdAt: Date.now()
-    }
-  };
+    TableName: process.env.tableName,
+  Item: {
+    userId: event.requestContext.identity.cognitoIdentityId,
+    noteId: uuid.v1(),
+    content: data.content,
+    attachment: data.attachment,
+    createdAt: Date.now()
+  }
+};
+
 
   try {
     await dynamoDbLib.call("put", params);
